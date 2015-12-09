1ed022aa664f4560c9f51a3c3b59759656b9bf47
Benjamin Kiessling <mittagessen@l.unchti.me>
2015-12-09 18:34:57 +0100

    Extend approximate_median to work with confidences
    Include a new symbol-based weighted distance metric that can be coerced
    to work with the greedy approximation algorithm. The improvement
    algorithm is still missing.

--- nidaba/algorithms/median.py
+++ nidaba/algorithms/median.py
@@ -16,9 +16,10 @@ import operator
 import unicodedata
 import itertools
 import math
+import collections
 
 from nidaba.nidabaexceptions import NidabaAlgorithmException
-from nidaba.algorithms.string import edit_distance
+from nidaba.algorithms.string import full_edit_distance, edit_distance
 
 # stolen from kraken
 class ocr_record(object):
@@ -40,7 +41,19 @@ class ocr_record(object):
         self.idx = -1
         return self
 
-    def __next__(self):
+    def __add__(self, other):
+        new = ocr_record(self.prediction, self.cuts, self.confidences)
+        if isinstance(other, collections.Iterable):
+            new.prediction += other[0]
+            new.cuts.extend(other[1])
+            new.confidences.extend(other[2])
+        elif isinstance(other, ocr_record):
+            new.prediction += other.prediction
+            new.cuts += other.cuts
+            new.confidences += other.confidences
+        return new
+
+    def next(self):
         if self.idx + 1 < len(self):
             self.idx += 1
             return (self.prediction[self.idx], self.cuts[self.idx],
@@ -63,15 +76,39 @@ class ocr_record(object):
         else:
             raise TypeError('Invalid argument type')
 
+class confidence_weighted_edit_distance(object):
+    """
+    Edit distance function using symbol-based confidence weighted edit
+    distance to an unweighted input string adapted from [0].
 
-def naive_edit_distance(string, record):
+    [0] Barat, Cécile, et al. "Weighted symbols-based edit distance for
+    string-structured image classification." Machine Learning and Knowledge
+    Discovery in Databases. Springer Berlin Heidelberg, 2010. 72-86.
+    """
+    def __init__(self, strings):
+        pass
+
+    def distance(self, string, record):
+        def cost(tok1, tok2, *args, **kwargs):
+            if tok1[0] != tok2[0]:
+                return max(tok1[2], tok2[2])
+            else:
+                return abs(tok2[2] - tok1[2])
+        m = full_edit_distance(string, record, ins_func=cost, del_func=cost, sub_func=cost)[0]
+        return m[-1][-1]
+
+class naive_edit_distance(object):
     """
     Edit distance function using the unweighted Levenshtein distance measure.
     """
-    return edit_distance(string, record.prediction)
+    def __init__(self, strings):
+        pass
+
+    def distance(self, string, record):
+        return edit_distance(string.prediction, record.prediction)
 
 
-def approximate_median(strings, distance_func=naive_edit_distance):
+def approximate_median(strings, distance_class=naive_edit_distance):
     """
     Calculates an approximate generalized median string using the greedy
     algorithm described in [0].
@@ -89,13 +126,27 @@ def approximate_median(strings, distance_func=naive_edit_distance):
     alphabet = set()
     for x in strings:
         alphabet.update(set(x.prediction))
-    median = u''
+    median = ocr_record(u'', [], [])
     mediandist = [np.inf]
     maxlen = len(max(strings, key=len))
+    distance = distance_class(strings)
     while True:
         mm = np.inf
         for j in alphabet:
-            ma = median + j
+            confs = []
+            # A new symbol may correspond to all matching symbols in the input
+            # strings truncated to the length of the prefix + symbol. As
+            # matching potential new symbols based on the location on the page
+            # increases the input alphabet inordinately and requires an
+            # additional distance measure we just calculate the mean of all
+            # confidences so lower-than-average confidences in the input
+            # strings are still penalized.
+            for s in strings:
+                confs += [x[2] for x in s[:len(median)+1] if x[0] == j]
+            if not confs:
+                confs = [100]
+            ma = median + (j, [None], [np.mean(confs)])
+
             m = 0
             t = 0
             # XXX: horribly inefficient. Running the algorithm as intended
@@ -104,19 +155,18 @@ def approximate_median(strings, distance_func=naive_edit_distance):
             # iterations. As we have no knowledge of the operation of the
             # distance function there is no other way for now.
             for s in strings:
-                t += distance_func(ma, s)
-                m += distance_func(ma, s[:len(ma)])
+                m += distance.distance(ma, s[:len(ma)])
+                t += distance.distance(ma, s)
             if m < mm:
                 mm = m
                 b_ma = ma
-                b_dist = t
         mediandist.append(t)
         median = b_ma
         if len(median) > maxlen and mediandist[-1] > mediandist[-2]:
             return median[:-1]
 
 
-def improve_median(candidate, strings, distance_func=naive_edit_distance):
+def improve_median(candidate, strings, distance_class=naive_edit_distance):
     """
     Tries to improve an approximate generalized median string using iterative
     systematic perturbation as described in [0]
@@ -137,20 +187,21 @@ def improve_median(candidate, strings, distance_func=naive_edit_distance):
     for x in strings:
         alphabet.update(set(x.prediction))
     i = 0
-    min_dist = (candidate, sum([distance_func(candidate, s) for s in strings]))
+    distance = distance_class(strings)
+    min_dist = (candidate, sum([distance.distance(candidate, s) for s in strings]))
     while True:
         for sym in alphabet:
             # substitution
             sub = candidate[:i] + sym + candidate[i+1:]
-            dist = sum([distance_func(sub, s) for s in strings]) 
+            dist = sum([distance.distance(sub, s) for s in strings]) 
             min_dist = (sub, dist) if dist < min_dist[1] else min_dist
             # insertion
             ins = candidate[:i] + sym + candidate[i:]
-            dist = sum([distance_func(ins, s) for s in strings]) 
+            dist = sum([distance.distance(ins, s) for s in strings]) 
             min_dist = (ins, dist) if dist < min_dist[1] else min_dist
         # deletion
         _del = candidate[i+1:]
-        dist = sum([distance_func(_del, s) for s in strings])
+        dist = sum([distance.distance(_del, s) for s in strings])
         if dist < min_dist[1]:
             min_dist = (_del, dist)
             i -= 1
